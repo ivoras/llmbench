d35ca6d101d8684fb0c919001d6405b282dad973
Ivan Voras <ivoras@gmail.com>
2024-12-10 01:14:23 +0100

    Better help

--- llmbench.py
+++ llmbench.py
@@ -11,25 +11,27 @@ DEFAULT_LLAMA_CPP_VERSION = '4295'
 
 LLM_TEXT_MODEL_URL = 'https://huggingface.co/bartowski/Qwen2.5-3B-GGUF/resolve/main/Qwen2.5-3B-Q4_K_L.gguf'
 
-bench_config: BenchConfig = None
 
 def main():
-    process_args()
+    bench_config = process_args()
+    print(bench_config)
 
 
 def process_args():
-    global bench_config
     parser = argparse.ArgumentParser(
-        prog='llmbench'
+        prog='llmbench',
+        epilog=f"See the Assets section in llama.cpp Releases (https://github.com/ggerganov/llama.cpp/releases) for what engines, OSes and CPU architectures are supported.\nDefault={DEFAULT_ENGINE_LIST} {DEFAULT_ENGINE_OS} {DEFAULT_ENGINE_ARCH}.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    engine_list = DEFAULT_ENGINE_LIST[:]
+    engine_list = []
 
-    parser.add_argument('-E', '--enable', action='append')
-    parser.add_argument('-D', '--disable', action='append')
-    parser.add_argument('-O', '--os', default=DEFAULT_ENGINE_OS)
-    parser.add_argument('-A', '--arch', default=DEFAULT_ENGINE_ARCH)
-    parser.add_argument('-L', '--llama_cpp_version', default=DEFAULT_LLAMA_CPP_VERSION)
+    parser.add_argument('-E', '--enable', action='append', default=DEFAULT_ENGINE_LIST, help='Enable an llama.cpp engine')
+    parser.add_argument('-D', '--disable', action='append', help='Disable an llama.cpp engine')
+    parser.add_argument('-O', '--os', default=DEFAULT_ENGINE_OS, help='Operating system')
+    parser.add_argument('-A', '--arch', default=DEFAULT_ENGINE_ARCH, help='CPU architecture')
+    parser.add_argument('-L', '--llama_cpp_version', default=DEFAULT_LLAMA_CPP_VERSION, help='llama.cpp release version')
+    parser.add_argument('-d', '--working_dir', default='.', help='Working directory (5-10 GB of disk space needed)')
     args = parser.parse_args()
     if args.enable:
         for engine in args.enable:
@@ -44,8 +46,9 @@ def process_args():
         engine_os = args.os,
         engine_arch = args.arch,
         llama_cpp_version = args.llama_cpp_version,
+        working_dir = args.working_dir,
     )
-    print(bench_config)
+    return bench_config
 
 
 if __name__ == '__main__':
